dd66187715043a662c2556e27d1cdac39a8c1d26
byunjuneseok <byunjuneseok@gmail.com>
2022-03-30 03:52:42 +0900

    Add the implementation of `FixedWindowCounter`

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, APIRouter, HTTPException
 
-from frl.algorithms import SimpleAlgorithm
+from frl.algorithms import SimpleAlgorithm, FixedWindowCounter, WindowSize
 from frl.backend import LimiterBackend
 from frl.key import NoKey, KeyByPath
 from frl.limiter import Limiter
@@ -21,36 +21,57 @@ async def root():
     return {'message': 'Hello World'}
 
 
-api_apple_limiter = Limiter(
-    name='api_apple_limiter',
+api_fruit_limiter = Limiter(
+    name='api_fruit_limiter',
     backend=limiter_backend,
     algorithm=SimpleAlgorithm(threshold=10),
     key_generator=KeyByPath(),
     exception=HTTPException(status_code=503, detail='Too many requests for Apples API. (10 hits per minutes allowed)')
 )
-api_apple_router = APIRouter(prefix='/apples', tags=['Apple'], dependencies=[Depends(api_apple_limiter)])
+api_fruit_router = APIRouter(prefix='/fruits', tags=['Apple'], dependencies=[Depends(api_fruit_limiter)])
 
 
-@api_apple_router.get('/')
-async def list_apples():
+@api_fruit_router.get('/')
+async def list_fruits():
     return [
         {
-            'apple_id': 3,
-            'apple_name': 'Strawberry flavoured apple'
+            'fruit_id': 3,
+            'fruit_name': 'Strawberry flavoured apple'
         },
         {
-            'apple_id': 5,
-            'apple_name': 'Durian flavoured apple'
+            'fruit_id': 5,
+            'fruit_name': 'Durian flavoured banana'
         }
     ]
 
 
-@api_apple_router.post('/{apple_id}/', status_code=201)
-async def create_apples():
+@api_fruit_router.post('/{fruit_id}/', status_code=201)
+async def create_fruits():
     return {
-        'apple_id': '6',
-        'apple_name': 'iMac with M1 Ultra'
+        'fruit_id': '6',
+        'fruit_name': 'iMac with M1 Ultra'
     }
 
 
-app.include_router(api_apple_router)
+app.include_router(api_fruit_router)
+
+
+api_animal_limiter = Limiter(
+    name='api_animal_limiter',
+    backend=limiter_backend,
+    algorithm=FixedWindowCounter(WindowSize.SECOND, 3),
+    key_generator=KeyByPath(),
+)
+api_animal_router = APIRouter(prefix='/animals', tags=['Animals'], dependencies=[Depends(api_animal_limiter)])
+
+
+@api_animal_router.get('/{animal_id}/')
+async def get_fruits(animal_id: str):
+
+    return {
+        'requested_id': animal_id,
+        'animal_id': f'#{animal_id} - Monkey'
+    }
+
+
+app.include_router(api_animal_router)
--- frl/algorithms.py
+++ frl/algorithms.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+from time import time
 
 from frl.backend import LimiterBackend
 
@@ -28,5 +30,44 @@ class SimpleAlgorithm(BaseAlgorithm):
 
         if requests == 1:
             await redis.expire(key, 60)
+
+        return requests <= self._threshold
+
+
+class WindowSize(Enum):
+    SECOND = 1
+    MINUTE = 60
+    HOUR = 60 * 60
+    DAY = 60 * 60 * 24
+
+
+class FixedWindowCounter(BaseAlgorithm):
+    _threshold: int
+    _window_size: int
+
+    def __init__(self, window_size: WindowSize, threshold: int):
+        if not isinstance(threshold, int):
+            raise ValueError('The threshold must be a positive integer.')
+
+        if threshold < 0:
+            raise ValueError('The threshold must be a positive integer.')
+
+        self._threshold = threshold
+
+        if not isinstance(window_size, WindowSize):
+            raise ValueError('\"window_size\" is invalid.')
+
+        self._window_size = window_size.value
+
+    async def request(self, key: str, backend: LimiterBackend, **kwargs) -> bool:
+
+        key_postfix = int(time() / self._window_size)
+        key = f'{key}::{key_postfix}'
+
+        async with backend.get_client() as redis:
+            requests = await redis.incr(key)
+
+        if requests == 1:
+            await redis.expire(key, self._window_size)
 
         return requests <= self._threshold
